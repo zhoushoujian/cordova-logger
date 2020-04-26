74f86c1abb357173924759845f67f825b51aab1e
zhoushoujian <szhou@zippia.com>
2020-04-26 14:50:32 +0800

    fix some log was ignored

--- cordova-logger.js
+++ cordova-logger.js
@@ -85,50 +85,106 @@
 			throw new Error("^^^^^^^^cordova-logger: please install cordova-plugin-file first")
 		}
 		return new Promise(async function (res) {
-			if(self.cordovaFileWrite){
-				// cache cordova file writer api
-				const result = await writeFile(self.cordovaFileWrite, data)
-				return res(result)
-			} else {
-				return self.requestFileSystem(self.LocalFileSystem.PERSISTENT, 0, function (fs) {
-					fs.root.getDirectory(folder, {
+			return self.requestFileSystem(self.LocalFileSystem.PERSISTENT, 0, function (fs) {
+				fs.root.getDirectory(folder, {
+					create: true
+				}, function (dirEntry) {
+					dirEntry.getDirectory(column, {
 						create: true
-					}, function (dirEntry) {
-						dirEntry.getDirectory(column, {
-							create: true
-						}, function (subDirEntry) {
-							subDirEntry.getFile(
-								filename, {
-									create: true,
-									exclusive: false
-								},
-								function (fileEntry) {
-									fileEntry.createWriter(async function (fileWriter) {
-										self.cordovaFileWrite = fileWriter
-										const result = await writeFile(fileWriter, data)
-										res(result)
-									});
-								}, onErrorCreateFile);
-						}, onErrorLoadFs);
-					}, onErrorGetDir);
+					}, function (subDirEntry) {
+						subDirEntry.getFile(
+							filename, {
+								create: true,
+								exclusive: false
+							},
+							function (fileEntry) {
+								fileEntry.createWriter(async function (fileWriter) {
+									fileWriter.onwriteend = function () {
+										res(data)
+										self.logger.queue.shift()
+										if(self.logger.queue.length){
+											return createAndWriteFile(self.logger.queue.slice(0, 1)[0], folder, column, filename)
+										}
+									};
+									fileWriter.onerror = function (e) {
+										console.error("cordova-logger: write file fail", e);
+										res(e)
+										self.logger.queue.shift()
+										if(self.logger.queue.length){
+											return createAndWriteFile(self.logger.queue.slice(0, 1)[0], folder, column, filename)
+										}
+									};
+									fileWriter.seek(fileWriter.length);
+									fileWriter.write(data);
+								});
+							}, onErrorCreateFile);
+					}, onErrorLoadFs);
 				}, onErrorGetDir);
+			}, onErrorGetDir);
+		})
+		.catch(err => {
+			console.error("cordova-logger createAndWriteFile pronmise err", err)
+			self.logger.queue.shift()
+			if(self.logger.queue.length){
+				return createAndWriteFile(self.logger.queue.slice(0, 1), folder, column, filename)
 			}
-
 		})
 	}
 
-	function writeFile(fileWriter, data){
-		return new Promise((resolve) => {
-			fileWriter.onwriteend = function () {
-				resolve(data)
+	function Logger(config) {
+		if (config === undefined || Object.prototype.toString.call(config) === '[object Object]') {
+			self.userConfig = (config || {})
+			self.userConfig.folder = (typeof (self.userConfig.folder) === 'string' ? self.userConfig.folder : "cordova-logger")
+			self.userConfig.column = (typeof (self.userConfig.column) === 'string' ? self.userConfig.column : "log")
+			self.userConfig.filename = (typeof (self.userConfig.filename) === 'string' ? self.userConfig.filename : "logger.log")
+			self.logger = {
+				userConfig: self.userConfig,
+				queue: []
 			};
-			fileWriter.onerror = function (e) {
-				console.error("cordova-logger: write file fail" + JSON.stringify(e));
-				resolve(e)
-			};
-			fileWriter.seek(fileWriter.length);
-			fileWriter.write(data);
-		})
+			LOGGER_LEVEL.forEach(item => {
+				self['logger'][item] = (buffer = "", ...args) => {
+					const param = [buffer, ...args]
+					if (item === 'debug') {
+						console.log(`[${getTime()}] [DEBUG]`, buffer, ...args);
+						return Promise.resolve(...param)
+					} else {
+						loopTimes = 0
+						console[item](`[${getTime()}] [${item.toUpperCase()}] `, buffer, ` [ext]`, ...args)
+						if (isAndroidApp) {
+							const {
+								folder,
+								column,
+								filename
+							} = self.userConfig
+							buffer = JSON.stringify(buffer, function (key, value) {
+								return formatDataType(value)
+							}, 4)
+							let extend = [];
+							if (args.length) {
+								extend = args.map(item => dealWithItems(item));
+								if (extend.length) {
+									extend = `  [ext] ${extend.join("")}`;
+								}
+							}
+							const content = `${buffer}` + `${extend}` + "\r\n";
+							const rawData = `[${getTime()}] [${item.toUpperCase()}] ${content}`;
+							if(!self.logger.queue.length){
+								self.logger.queue.push(rawData)
+								return createAndWriteFile(rawData, folder, column, filename);
+							} else {
+								return self.logger.queue.push(rawData)
+							}
+						} else {
+							console.log(`[${getTime()}] [${item.toUpperCase()}]`, buffer, ...args);
+							return Promise.resolve(...param)
+						}
+					}
+				}
+			})
+			return self.logger
+		} else {
+			throw new Error("^^^^^^^^cordova-logger config must be an object")
+		}
 	}
 
 	function onErrorCreateFile(error) {
@@ -176,55 +232,6 @@
 		}
 		const time = `${year}-${month}-${day} ${hour}:${minute}:${second}.${mileSecond}`;
 		return time;
-	}
-
-	function Logger(config) {
-		if (config === undefined || Object.prototype.toString.call(config) === '[object Object]') {
-			self.userConfig = (config || {})
-			self.userConfig.folder = (typeof (self.userConfig.folder) === 'string' ? self.userConfig.folder : "cordova-logger")
-			self.userConfig.column = (typeof (self.userConfig.column) === 'string' ? self.userConfig.column : "log")
-			self.userConfig.filename = (typeof (self.userConfig.filename) === 'string' ? self.userConfig.filename : "logger.log")
-			self.logger = {};
-			LOGGER_LEVEL.forEach(item => {
-				self['logger'][item] = (buffer = "", ...args) => {
-					const param = [buffer, ...args]
-					if (item === 'debug') {
-						console.log(`[${getTime()}] [DEBUG]`, buffer, ...args);
-						return Promise.resolve(...param)
-					} else {
-						loopTimes = 0
-						console[item](`[${getTime()}] [${item.toUpperCase()}] `, buffer, ` [ext]`, ...args)
-						if (isAndroidApp) {
-							const {
-								folder,
-								column,
-								filename
-							} = self.userConfig
-							buffer = JSON.stringify(buffer, function (key, value) {
-								return formatDataType(value)
-							}, 4)
-							let extend = [];
-							if (args.length) {
-								extend = args.map(item => dealWithItems(item));
-								if (extend.length) {
-									extend = `  [ext] ${extend.join("")}`;
-								}
-							}
-							const content = `${buffer}` + `${extend}` + "\r\n";
-							const rawData = `[${getTime()}] [${item.toUpperCase()}] ${content}`;
-							return createAndWriteFile(rawData, folder, column, filename);
-						} else {
-							console.log(`[${getTime()}] [${item.toUpperCase()}]`, buffer, ...args);
-							return Promise.resolve(...param)
-						}
-					}
-				}
-			})
-			self.logger.userConfig = self.userConfig
-			return self.logger
-		} else {
-			throw new Error("^^^^^^^^cordova-logger config must be an object")
-		}
 	}
 
 	Logger.prototype.checkFileWritePriority = function () {
